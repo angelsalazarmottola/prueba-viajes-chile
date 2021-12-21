e038dec0c9f5b38b203cdf28359797ed9097781f
angelsalazarmottola <angelsalazarmottola@gmail.com>
2021-12-20 22:50:44 -0300

    se agregó js al nav bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,14 @@ $('h2').click(function () {
 $(".texto-footer").hover(function(){
     $(this).html("<h3> Encuéntranos en nuestras redes sociales! <h3/>");
     });
+
+
+$(window).scroll(function(){
+    if( $(this).scrollTop() > 100){
+        	$("nav").removeClass('bg-transparent');
+            $("nav").addClass('bg-primary');
+    }else{
+        $("nav").removeClass('bg-primary');
+        $("nav").addClass("bg-transparent");
+    }
+})
